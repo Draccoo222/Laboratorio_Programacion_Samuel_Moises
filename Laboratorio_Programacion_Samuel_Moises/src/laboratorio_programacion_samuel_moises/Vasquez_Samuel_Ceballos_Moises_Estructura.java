cfcc7eb7c95d752c7568537c830c9e1191d3094a
moisesceballos <ceballosruiz2711@gmail.com>
2025-05-16 14:54:43 -0600

    OPCION FILTRAR TERMINADA

--- Laboratorio_Programacion_Samuel_Moises/src/laboratorio_programacion_samuel_moises/Vasquez_Samuel_Ceballos_Moises_Estructura.java
+++ Laboratorio_Programacion_Samuel_Moises/src/laboratorio_programacion_samuel_moises/Vasquez_Samuel_Ceballos_Moises_Estructura.java
@@ -20,15 +20,67 @@ public class Vasquez_Samuel_Ceballos_Moises_Estructura {
                 case 1: // Cifrado Cesar
                     break;
                 case 2: // Filtrar
+                    System.out.println("---Filtrar---"); //CORRECIONES (MOSTRAR TEXTO SI NO SE ENCUENTRN , AGREGAR MAS SIGNOS A IGNORAR)
+                    
+                    //VARIABLES
+                    
+                    int longitudMin = -1;
+                    char letra =' ';
+                    String palabra = "";
+                          
+                    System.out.print("Ingrese una frase: "); //PIDE FRASE
+                    String frase = entrada.next();
+                    
+                    
+                    while (longitudMin<0){ //CICLO QUE VALIDA SI ES NUMERO POSITIVO
+                        System.out.print("Ingrese un numero positivo para la longitud minima requerida: ");
+                        longitudMin = entrada.nextInt();
+                    }
+                                        
+                    System.out.println("Palabras cuya longitud es mayor a " + longitudMin + ": ");
+                    
+                    for (int index = 0; index<frase.length();index++){
+                        
+                        letra = frase.charAt(index);
+                        if (letra!=' ' && letra!=',' && letra !='.' && letra!= ':'){  //CONSTRUYE LA PALABRA SI NO ENCUENTRA UN ESPACIO
+                       
+                            palabra+=letra;
+                        
+                        } else { 
+                            
+                            if (palabra.length() > longitudMin){ //VALIDA SI LA PALABRA ES MAYOR A LA LONGITUD INGRESADA
+                            System.out.println(palabra);
+                            
+                            
+                            }
+                            palabra="";   //REINICIA LA PALABRA 
+                            
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                    }
+                        
+                    
+                    
+                    
+                    
+                    
+                    
                     break;
                 case 3: // Codigo Enigma
-                    System.out.println("aaaa");
+                   
                     break;
                 default: //Opcion erronea
                     System.out.println("Porfavor, solo introduzca las opciones validas.");
                     break;
             }
-        }while(opc > 0 && opc < 4);
+            
+        }while(opc != 1 && opc !=2 && opc!=3); 
         
        
     }
