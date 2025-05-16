62721d3684f0d652265b130184a2b00042f80400
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-05-16 14:54:55 -0600

    Cifrado Cesar
    se termina la opcion de cifrado cesar

--- Laboratorio_Programacion_Samuel_Moises/src/laboratorio_programacion_samuel_moises/Vasquez_Samuel_Ceballos_Moises_Estructura.java
+++ Laboratorio_Programacion_Samuel_Moises/src/laboratorio_programacion_samuel_moises/Vasquez_Samuel_Ceballos_Moises_Estructura.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class Vasquez_Samuel_Ceballos_Moises_Estructura {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in).useDelimiter("\n");
         int opc;
       
         do{
-            System.out.println("-------Menu-------");
+            System.out.println("\n-------Menu-------");
             System.out.println("[1] Cifrado Cesar");
             System.out.println("[2] Filtrar");
             System.out.println("[3] Codigo Enigma");
@@ -18,6 +18,32 @@ public class Vasquez_Samuel_Ceballos_Moises_Estructura {
             opc = entrada.nextInt();
             switch(opc){
                 case 1: // Cifrado Cesar
+                    System.out.println("\n****Cifrado Cesar****");
+                    // Se le pide al usuario que introduzca una frase
+                    System.out.print("Introduzca la frase a cifrar: ");
+                    String frase = entrada.next();
+                    // Se le pide la cantidad de desplazamiento que quiere
+                    System.out.print("Introduzca el desplazamiento (en numeros enteros): ");
+                    int desplaz = entrada.nextInt();
+                    // Se asegura que el desplazamiento sea segun la cantidad de numeros en el abecedario
+                    desplaz = desplaz % 26;
+                    
+                    String resultado = "";
+                    
+                    for (int i = 0; i < frase.length(); i++) {
+                      char letra =  frase.charAt(i);
+                      if(Character.isLetter(letra)){ // Se verifica que los caracteres dentro de la frase sean letras para poder encriptarlos
+                          char base = (Character.isUpperCase(letra)) ? 'A' : 'a'; // Se usa para asegurar que las letras mayuscula y minuscula no sean confundidas
+                          char cifrado = (char) ((letra - base + desplaz + 26)%26 + base); /* Hace la conversion tipo casting de la letra para 
+                          obtener su posicion y asi cifrara segun el desplazamiento*/
+                          resultado += cifrado; // Despues de cifrar se suma la letra cifrada al resultado, que es la palabra modificada 
+                      }else{
+                          resultado += letra; // Si es un signo de puntuacion o un espacio se suma sin modificaro
+                      }
+                    }
+                    System.out.println("---- RESULTADO ----");
+                    System.out.println(resultado);
+                    System.out.println("-------------------");
                     break;
                 case 2: // Filtrar
                     break;
